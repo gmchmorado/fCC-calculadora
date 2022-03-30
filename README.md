2c07ce5dc2451ae6eeff508ac53895aec824cf1e
gmchmorado <gmchmorado@gmail.com>
2022-03-30 17:14:09 -0600

    Proyecto concluido, test aprobados

--- README.md
+++ README.md
@@ -8,31 +8,31 @@ Codificar una calculadora básica.
 ---
 ## Elementos de evaluación
 
-- Debe tener un elemento, en el que se pueda hacer clic, que represente al signo `=` con un `id=equal`
+- Debe tener un elemento, en el que se pueda hacer clic, que represente al signo `=` con un `id = equal`
 - Debe tener diez elementos, en los que se pueda hacer clic, que representen a los números del 0 al 9 con los siguientes identificadores únicos:
-  - `id-zero`
-  - `id-one`
-  - `id-two`
-  - `id-three`
-  - `id-four`
-  - `id-five`
-  - `id-six`
-  - `id-seven`
-  - `id-eight`
-  - `id-nine`
+  - `id = zero`
+  - `id = one`
+  - `id = two`
+  - `id = three`
+  - `id = four`
+  - `id = five`
+  - `id = six`
+  - `id = seven`
+  - `id = eight`
+  - `id = nine`
 - Debe tener cuatro elementos, en los que se pueda hacer clic, que representen a los operadores matemáticos con los siguientes identificadores únicos:
-  - `id-add`
-  - `id-subtract`
-  - `id-multiply`
-  - `id-divide`
-- Debe tener un elemento, en el que se pueda hacer clic, que represente al punto decimal `.` con un `id=decimal`
-- Debe tener un elemento, en el que se pueda hacer clic, con un `id=clear`
-- Debe tener un elemento para mostrar valores, con un `id-display`
+  - `id = add`
+  - `id = subtract`
+  - `id = multiply`
+  - `id = divide`
+- Debe tener un elemento, en el que se pueda hacer clic, que represente al punto decimal `.` con un `id = decimal`
+- Debe tener un elemento, en el que se pueda hacer clic, con un `id = clear`
+- Debe tener un elemento para mostrar valores, con un `id = display`
 - En todo momento, si se hace clic en el botón `clear` se eleminaran los valores de entrada y salida, regresar a la calculadora al estado inicial y el elemento `display` debe mostrar `0`
 - Al introducir los números se deben reflejar en el elemento `display`
 - Deberá ser posible ingresar los operadores mátematicos en cualquier momento con números de cualquier longitud; al presionar el signo `=` se deberá mostrar el resultado en el elemento `display`
 - Al introducir números no se debe permir que el número comience con uno o varios `0`
-- Cuando se haga clic en el elemento `.` se deberá agregar el punto decima; no se deberán acepart dos puntos en un mismo número
+- Cuando se haga clic en el elemento `.` se deberá agregar el punto decima; no se deberán aceptar dos puntos en un mismo número
 - La aplicación debe ser capza de operar cualquier operación matemática con números que contengan decimales.
 - Si se introducen dos o más operadores consecutivamente se deberá reconocer la operación del úlimo operador ingresado, con excepción del signo `-` que se reconocerá como cambio se signo del siguiente número.
 - Si se ingresa un operador inmediatamente después de signo `=` se debe comenzar un nuevo cálculo con el resultado anterior como primer elemento a operar
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 import './index.css';
 
 ReactDOM.render(
@@ -9,3 +11,132 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+function App() {
+  const [input,setInput] = useState('0');
+
+  const upDateDisplay = n => {
+    setInput((input === '0' ? n : input + n));
+  };
+
+  const dotInput = (n) => {
+    if (input.lastIndexOf('.') < 0) {
+      setInput(input + n);
+    } else if (
+      (input.lastIndexOf('+') >= 0) && (input.lastIndexOf('.') < input.lastIndexOf('+')) ||
+      (input.lastIndexOf('-') >= 0) && (input.lastIndexOf('.') < input.lastIndexOf('-')) ||
+      (input.lastIndexOf('*') >= 0) && (input.lastIndexOf('.') < input.lastIndexOf('*')) ||
+      (input.lastIndexOf('/') >= 0) && (input.lastIndexOf('.') < input.lastIndexOf('/'))
+    ) { 
+      setInput(input + n);
+    }
+  };
+
+  const doOperation = (n) => {
+    let busca = /[\+\-\*\/]$/;
+    let busca2 = /[\+\-\*\/].$/;
+    if ( (n === '+' || n === '*' || n === '/') && busca.test(input) ) {
+      if ( input[input.length - 1] === '-' && busca2.test(input)) {
+        setInput(input.slice(0,input.length-2) + n);
+      } else {
+        setInput(input.slice(0,input.length-1) + n);
+      }
+    } else {
+      setInput(input + n);
+    }
+  }
+
+  const result = () => {
+    setInput(evaluate(input));
+  };
+
+  return (
+    <div className='main-container'>
+      <div className='fila'>
+        <Display input={input}/>
+      </div>
+      <div className='fila'>
+        <Boton presionarBoton={upDateDisplay}>1</Boton>
+        <Boton presionarBoton={upDateDisplay}>2</Boton>
+        <Boton presionarBoton={upDateDisplay}>3</Boton>
+        <Boton presionarBoton={doOperation}>+</Boton>
+      </div>
+      <div className='fila'>
+        <Boton presionarBoton={upDateDisplay}>4</Boton>
+        <Boton presionarBoton={upDateDisplay}>5</Boton>
+        <Boton presionarBoton={upDateDisplay}>6</Boton>
+        <Boton presionarBoton={upDateDisplay}>-</Boton>
+      </div>
+      <div className='fila'>
+        <Boton presionarBoton={upDateDisplay}>7</Boton>
+        <Boton presionarBoton={upDateDisplay}>8</Boton>
+        <Boton presionarBoton={upDateDisplay}>9</Boton>
+        <Boton presionarBoton={doOperation}>*</Boton>
+      </div>
+      <div className='fila'>
+        <Boton presionarBoton={upDateDisplay}>0</Boton>
+        <Boton presionarBoton={dotInput}>.</Boton>
+        <Boton presionarBoton={result}>=</Boton>
+        <Boton presionarBoton={doOperation}>/</Boton>
+      </div>
+      <div className='fila'>
+        <Clear borrarPantalla={() => setInput('0')}>CLEAR</Clear>
+      </div>
+    </div>
+  )
+};
+
+function Display({ input }) {
+  return(
+    <div 
+      className='display'
+      id='display'>
+      {input}
+    </div>
+  );
+};
+
+function Boton(props) {
+  
+  const idBotones = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    '+': 'add',
+    '-': 'subtract',
+    '*': 'multiply',
+    '/': 'divide',
+    '.': 'decimal',
+    '=': 'equals'
+  };
+
+  const mathOp = (contenido) => {
+    return isNaN(contenido);
+  }
+
+  return (
+    <div
+      id={idBotones[props.children]} 
+      className={`boton-container ${mathOp(props.children) ? 'math-operator' : ''}`.trimEnd()} 
+      onClick={() => props.presionarBoton(props.children)}>
+      {props.children}
+    </div>
+  );
+};
+
+function Clear(props) {
+  return(
+    <div 
+      id='clear' 
+      className='boton-clear'
+      onClick={props.borrarPantalla}>
+      {props.children}
+    </div>
+  )
+};
